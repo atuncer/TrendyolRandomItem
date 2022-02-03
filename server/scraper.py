ea3e356befc15c40a9189defce76846d49e417ef
atuncer <atahant99@gmail.com>
2022-02-03 15:42:57 +0300

    organized directories

---
+++ server/scraper.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import json
+import requests
+
+
+def getSite(url):
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'}
+    result = requests.get(url, headers=headers)
+    return result
+
+def getLinks(html):
+    dic = {}
+    soup = BeautifulSoup(html, "html.parser")
+
+    for tablink in soup.find_all("li", {"class": "tab-link"}):
+        dic[tablink.find("a").text] = [{}]
+        for ul in tablink.find_all("ul"):
+            for li in ul.find_all("li"):
+                dic[tablink.find("a").text][0][li.find("a").text] = li.find("a")["href"]
+
+    return dic
+
+def writeJson(jso):
+    json_object = json.dumps(jso, indent=2, ensure_ascii=False)
+
+    with open("allCategories.json", "w", encoding="utf-8") as outfile:
+        outfile.write(json_object)
+
+    with open("bigCategories.json", "w", encoding="utf-8") as outfile:
+        text = "["
+        for key in jso:
+            text += "\n\t\"" + key + "\","
+        text = text[:-1] + "\n]"
+        outfile.write(text)
+    
+
+if __name__ == "__main__":
+    URL = "https://trendyol.com/"
+    html = getSite(URL).text
+    jso = getLinks(html)
+    writeJson(jso)
